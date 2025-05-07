0224a6f096b24c2c5fb845b3857e7ade1741f9f4
Gabriela <gabifmouraroc@outlook.com>
2025-05-07 19:53:44 -0300

    propriedade 'priority' no header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <Box bg={"#F2C041"} paddingY={"1rem"}>
             <Flex alignItems={"center"} justifyContent={"center"}>
-                <Image src="https://static.wixstatic.com/media/edb4f9_abd0533208c44e4e84bdc4ad942ecbb6~mv2.png/v1/fill/w_160,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo-fm2.png" alt="logo-fm2.png" width="160" height="60" />
+                <Image src="https://static.wixstatic.com/media/edb4f9_abd0533208c44e4e84bdc4ad942ecbb6~mv2.png/v1/fill/w_160,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/logo-fm2.png" alt="logo-fm2.png" width="160" height="60" priority />
             </Flex>
         </Box>
     )

5180e7062e88b9274fc57f6f013a7615eb6f8c7c
Gabriela <gabifmouraroc@outlook.com>
2025-05-09 14:22:54 -0300

    mudança na ordem das tarefas e exibição de mensagem quando não existir tarefas

--- src/components/allTasks/AllTasks.tsx
+++ src/components/allTasks/AllTasks.tsx
@@ -1,18 +1,29 @@
 "use client"
 
 import { useAppContext } from "@/contexts/AppContext";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import { Task } from "../task/Task";
 
 export function AllTasks() {
     const { tasks } = useAppContext()
+
+    const tasksInOrder = [...tasks].sort(
+        (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+
     return (
-        <Flex gap={"1rem"} direction={{ base: "column", md: "row" }}>
-            {tasks.map((task) => (
-                <Task key={task.id} task={task} />
-
-            ))}
-
-        </Flex>
+        <>
+            {tasks.length > 0 ? (
+                <Flex gap={"1rem"} direction={{ base: "column", md: "row" }}>
+                    {tasksInOrder.map((task) => (
+                        <Task key={task.id} task={task} />
+                    ))}
+                </Flex>
+            ) : (
+                <Flex justifyContent={"center"} alignItems={"center"}>
+                    <Text textStyle={"2xl"} fontWeight={"semibold"} color={"yellow.500"}>Não há tarefas salvas. Crie uma nova!</Text>
+                </Flex>
+            )}
+        </>
     )
 }
